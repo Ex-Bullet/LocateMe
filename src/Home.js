245c723624313815c88d8ec2901f376d7d98806e
Eyyup <huakq@protonmail.com>
2019-08-29 19:54:03 +0200

    map fixed and login semi fixed

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { View, Platform, Text, StyleSheet } from "react-native";
 import MapView from 'react-native-maps';
-import Constants from 'expo-constants';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
 import { Marker } from 'react-native-maps';
 
 
@@ -11,74 +8,46 @@ export default class HomeScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            location: null,
-            errorMessage: null,
+            pn: this.props.navigation.getParam('pn'),
+            lnt: this.props.navigation.getParam('lnt'),
+            lng: this.props.navigation.getParam('lng')
           };
-        }
 
-    componentWillMount() {
-        if (Platform.OS === 'android' && !Constants.isDevice) {
-          this.setState({
-            errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-          });
-        } else {
-          this._getLocationAsync();
         }
-      }
-    
-      _getLocationAsync = async () => {
-        let { status } = await Permissions.askAsync(Permissions.LOCATION);
-        if (status !== 'granted') {
-          this.setState({
-            errorMessage: 'Permission to access location was denied',
-          });
-        }
-    
-        let location = await Location.getCurrentPositionAsync({});
-        this.setState({ location });
-      };
-      
+      // componentDidMount(){
+      //   console.log(this.state.pn);
+        
+      // }
     render() {
 
-        let cord = this.state.location;
-        let text = 'Waiting..';
-        if (this.state.errorMessage) {
-            console.log(this.state.errorMessage);
-             text = this.state.errorMessage;
-          } else if (this.state.location) {
-            console.log(this.state.location);    
-             text = JSON.stringify(this.state.location);
-          }
       return (
+        
         <View style ={styles.container}>
-            <Text>{text}</Text>
-            {
-                cord?
-                <MapView
-          style={styles.map}
-        >
+        <MapView style={styles.map}>
+        <MapView
+          initialRegion={{
+            latitude: this.state.lnt,
+            longitude: this.state.lng
+          }}
+          />
         <Marker
-      coordinate={{latitude:cord.latitude, longitude:cord.longitude}}
-      title={"dgssdgs"}
+      coordinate={{
+        latitude: this.state.lnt,
+        longitude: this.state.lng
+      }}
+      title={"Ma position"}
       description={"test"}
       />
-        </MapView> : <Text>Waiting..</Text>
-            }
+
+        </MapView>
             
-        {/* <MapView
-          style={styles.map}
-          region={{
-            latitude: cord.latitude,
-            longitude: cord.longitude,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        </MapView> */}
       </View>
       );
     }
   }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
--- src/Login.js
+++ src/Login.js
@@ -1,164 +1,181 @@
-import React, { Component } from "react";  
-import { 
+import React, { Component } from "react";
+import {
   View,
-  StyleSheet, 
+  StyleSheet,
   SafeAreaView,
   Keyboard,
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
-  Text
-} from 'react-native';
-import {
-  Container,
-  Item,
-  Input,
-  Icon,
-  Button
-} from 'native-base';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
-import PhoneInput, { formatPhoneNumber, isValidPhoneNumber } from 'react-phone-number-input';
+  Text,
+  Alert
+} from "react-native";
+import { Container, Item, Input, Icon, Button } from "native-base";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
-
+const valide = /^(\+33\s[1-9]{8})|(0[1-9]\s{8})$/;
 
 export default class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      text: "",
       location: null,
-      errorMessage: null
+      errorMessage: null,
+      lnt: null,
+      latitude: null
     };
   }
 
 
   check = () => {
+
+    let checks = valide.test(this.state.text);
+
+    if (checks) {
+      fetch("https://locatemeapi.herokuapp.com/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          phoneNumber: this.state.text,
+          lat: this.state.location.coords.latitude,
+          lng: this.state.location.coords.longitude
+        })
+      })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.props.navigation.navigate ('Home', {
+            pn: this.state.text,
+            lnt: this.state.lnt,
+            lng: this.state.lng})
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+    else {
+      Alert.alert("error enculey")
+    }
     // console.log(this.state.text);
     // console.log(this.state.location);
-    fetch('https://locatemeapi.herokuapp.com/login', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        phoneNumber: this.state.text,
-        lat: this.state.location.latitude,
-        lng: this.state.location.longitude
-      })
-    })
-    .then((response) => response.json())
-    .then((responseData) => {
-        console.log("Resultat", responseData)
-    })
-      .catch((error) =>{
-      console.error(error);
-      }) 
-    };
+    // console.log(this.state.location.coords.latitude, this.state.location.coords.longitude);
 
-
+};
 
   componentDidMount() {
     this._getloc();
   }
 
-
   _getloc = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
-        if (status !== 'granted') {
-          this.setState({
-            errorMessage: 'Permission to access location was denied',
-          });
-        }
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+    }
     let location = await Location.getCurrentPositionAsync({});
     this.setState({ location });
-  }
+    this.setState({ lnt: location.coords.latitude});
+    this.setState({lng: location.coords.longitude});
 
-  
+  };
+
+
   render() {
+    
     return (
-      
       <SafeAreaView style={styles.container}>
-        <KeyboardAvoidingView style={styles.container} behavior='padding' enabled>
-          <View style={{alignItems: 'center'}}>
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior="padding"
+          enabled
+        >
+          <View style={{ alignItems: "center" }}>
             <Text>Locate Me</Text>
           </View>
 
-
-
-
-          <TouchableWithoutFeedback style={styles.container} onPress={Keyboard.dismiss}>
+          <TouchableWithoutFeedback
+            style={styles.container}
+            onPress={Keyboard.dismiss}
+          >
             <View style={styles.container}>
               <Container style={styles.infoContainer}>
                 <Item rounded style={styles.itemStyle}>
+                  <Icon active name="call" style={styles.iconStyle} />
                   <Icon
                     active
-                    name='call'
-                    style={styles.iconStyle}
-                  />
-                  <Icon
-                    active
-                    name='md-arrow-dropdown'
+                    name="md-arrow-dropdown"
                     style={[styles.iconStyle, { marginLeft: 0 }]}
                   />
-                  <Input 
-                    placeholder='+33'
-                    placeholderTextColor='#adb4bc'
-                    keyboardType={'phone-pad'}
-                    returnKeyType='done'
-                    autoCapitalize='none'
+                  <Input
+                    placeholder="+33"
+                    placeholderTextColor="#adb4bc"
+                    keyboardType={"phone-pad"}
+                    returnKeyType="done"
+                    autoCapitalize="none"
+                    maxLength={11}
                     autoCorrect={false}
                     secureTextEntry={false}
-                    style={styles.inputStyle}       
-                    onChangeText={(text) => this.setState({text})} 
-                    value={this.setState.text}           
+                    style={styles.inputStyle}
+                    onChangeText={text => this.setState({ text })}
+                    value={this.setState.text}
                   />
                 </Item>
               </Container>
             </View>
           </TouchableWithoutFeedback>
-          <View style={{alignItems: 'center',}}>
-        <Button style={{width:150, alignItems: 'center', justifyContent: 'center'}} primary onPress={this.check}>
-          <Text style={{fontWeight: "bold", color: 'white'}}>CONTINUER</Text>
+          <View style={{ alignItems: "center" }}>
+            <Button
+              style={{
+                width: 150,
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+              primary
+              onPress={this.check}
+            >
+              <Text style={{ fontWeight: "bold", color: "white" }}>
+                CONTINUER
+              </Text>
             </Button>
-        </View>
-         </KeyboardAvoidingView>
+          </View>
+        </KeyboardAvoidingView>
       </SafeAreaView>
-    )
+    );
   }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'column'
+    justifyContent: "center",
+    flexDirection: "column"
   },
   infoContainer: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
     height: 200,
     bottom: 250,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 30,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 30
   },
-  iconStyle: { 
-    color: '#4285F4',
+  iconStyle: {
+    color: "#4285F4",
     fontSize: 28,
     marginLeft: 15
   },
   itemStyle: {
-    marginBottom: 10,
+    marginBottom: 10
   },
   inputStyle: {
     flex: 1,
     fontSize: 17,
-    fontWeight: 'bold',
-    color: '#212121',
+    fontWeight: "bold",
+    color: "#212121"
   }
-})
+});
